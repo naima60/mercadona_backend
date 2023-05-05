4cd65c3fd8e5dd6743299d0a4e9542a0c5efda06
naima <cours.dev60@gmail.com>
2023-05-05 21:21:44 +0200

    Mise en page

--- product/views.py
+++ product/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from product.models import Produit
+from django.core.paginator import Paginator
 
 # Create your views here.
+#def index(request):
+    #  = Produit.objects.all()
+   #products = Produit.objects.filter(active='True')
+    #return render(request, 'produit/index.html', context={"products": products})
+
 def index(request):
-    # products = Produit.objects.all()
-    products = Produit.objects.filter(active='True')
-    return render(request, 'produit/index.html', context={"products": products})
-
-
+    list_products = Produit.objects.filter(active='True')
+    paginator = Paginator(list_products, 15) # 15 articles par page
+    page = request.GET.get('page')
+    products = paginator.get_page(page)
+    return render(request, 'produit/index.html', context={'products': products})
